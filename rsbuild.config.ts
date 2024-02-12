0361273fd79c12e96ae5f289e500071579da83c4
Sparta16 <adenslep@mail.ru>
2024-02-13 02:24:48 +0300

    svg icon

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
 
 export default defineConfig({
-	plugins: [ pluginReact() ],
+	plugins: [ pluginReact(), pluginSvgr() ],
 });
