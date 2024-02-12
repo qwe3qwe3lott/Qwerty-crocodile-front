47d1dc955d3a1527b221fb84850faa568486846a
A Kozlov <qwe3qwe3lott@gmail.com>
2024-02-12 23:04:24 +0300

    Login redirect if needed

--- src/auth/screens/LoginScreen.tsx
+++ src/auth/screens/LoginScreen.tsx
@@ -1,17 +1,18 @@
 import { memo, useCallback } from 'react';
 import { LoginPage } from '@auth/pages/LoginPage';
 import { useAuthStore } from '@auth/auth.store';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const LoginScreen = memo(() => {
 	const { login, setLogin } = useAuthStore();
 	const navigate = useNavigate();
+	const { pathname } = useLocation();
 
 	const handleSubmit = useCallback((login: string) => {
 		setLogin(login);
 
-		navigate('/');
-	}, []);
+		pathname === '/login' && navigate('/');
+	}, [ pathname, navigate ]);
 
 	return <LoginPage initialLogin={login} onSubmit={handleSubmit}/>;
 });
