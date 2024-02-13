264962e894b7fda036a8ff1ec9666981765b63b8
Sparta16 <adenslep@mail.ru>
2024-02-13 22:24:36 +0300

    Profile icon

--- src/crocodile/components/RoomUsersList.tsx
+++ src/crocodile/components/RoomUsersList.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react';
 import { roomStoreOwnerIdSelector, roomStoreUsersSelector, useRoomStore } from '@crocodile/crocodile.store';
+import { ProfileIcon } from '@common/ui/ProfileIcon';
 
 export const RoomUsersList = memo(() => {
 	const users = useRoomStore(roomStoreUsersSelector);
@@ -9,7 +10,8 @@ export const RoomUsersList = memo(() => {
 		<ul className="grid gap-2 w-full">
 			{users.map((user) => {
 				return <li key={user.id}
-					className="bg-white grid rounded-full py-2 px-4 w-full grid-cols-[1fr_min-content]">
+					className="bg-white grid rounded-full py-2 px-4 w-full h-12 grid-cols-[min-content_1fr_min-content] gap-2 items-center">
+					<ProfileIcon className="h-12 w-12 -mt-2 -mb-2 -ml-4 p-1 rounded-full border-amber-700 border-2 border-solid"/>
 					<span className="text-2xl truncate">{user.login}</span>
 					{user.id === ownerId && <span className="place-self-center">👑</span>}
 				</li>;
