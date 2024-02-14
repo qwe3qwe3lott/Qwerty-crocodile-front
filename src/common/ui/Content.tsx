4ed24d12819492d26e2c6895d94cb9971b7fe0a2
Sparta16 <adenslep@mail.ru>
2024-02-14 21:24:03 +0300

    drawing tools

--- src/common/ui/Content.tsx
+++ src/common/ui/Content.tsx
@@ -5,7 +5,7 @@ type Props = PropsWithChildren<{
 }>;
 
 export const Content = memo<Props>(({ children, className = '' }) => {
-	return <section className={`bg-indigo-500 rounded-3xl w-full h-full max-h-256 max-w-512 p-8 ${className}`}>
+	return <section className={`bg-indigo-500 shadow-[0_0_8px_2px_#c4c8cc] rounded-3xl w-full h-full max-h-256 max-w-512 p-8 ${className}`}>
 		{children}
 	</section>;
 });
