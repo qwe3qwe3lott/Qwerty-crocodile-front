e951bc2d96d0e47971c8906894a97eee26641a74
A Kozlov <qwe3qwe3lott@gmail.com>
2024-02-16 22:41:56 +0300

    timer improve

--- src/crocodile/components/RoomSidebarTimer.tsx
+++ src/crocodile/components/RoomSidebarTimer.tsx
@@ -1,14 +1,14 @@
-import { memo, useEffect, useRef } from 'react';
+import { memo, useLayoutEffect, useRef, useState } from 'react';
 import { TimerState } from '@crocodile/crocodile.entity';
 
 type Props = {
 	timerState: TimerState
 };
 
-export const RoomSidebarTimer = memo<Props>(({ timerState }) => {
+const Timer = memo<Props>(({ timerState }) => {
 	const progressRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
+	useLayoutEffect(() => {
 		if (!progressRef.current) return;
 
 		const timeDifference = new Date().getTime() - (timerState.startTime);
@@ -23,11 +23,15 @@ export const RoomSidebarTimer = memo<Props>(({ timerState }) => {
 
 		progressRef.current.style.transition = `all ${leftDuration}ms linear`;
 
-		setTimeout(() => {
+		const timeout = setTimeout(() => {
 			if (!progressRef.current) return;
 
 			progressRef.current.style.width = '100%';
 		}, 100);
+
+		return () => {
+			clearTimeout(timeout);
+		};
 	}, []);
 
 	return (
@@ -37,5 +41,28 @@ export const RoomSidebarTimer = memo<Props>(({ timerState }) => {
 				className="bg-green-600 h-full"
 			/>
 		</div>
+	);
+});
+
+export const RoomSidebarTimer = memo<Props>(({ timerState }) => {
+	const [ key, setKey ] = useState(new Date().getTime());
+
+	useLayoutEffect(() => {
+		const listener = () => {
+			setKey(new Date().getTime());
+		};
+
+		window.addEventListener('focus', listener);
+
+		return () => {
+			window.removeEventListener('focus', listener);
+		};
+	}, []);
+
+	return (
+		<Timer
+			key={key}
+			timerState={timerState}
+		/>
 	);
 });
